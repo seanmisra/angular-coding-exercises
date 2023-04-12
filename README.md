96980b3247cc2758a276948855484604e83a04aa
seanmisra <seanmisra30@gmail.com>
2023-04-12 15:38:12 -0400

    Exercise 20: rxjs combineLatest

--- README.md
+++ README.md
@@ -18,5 +18,6 @@
 17) Demonstrate a use case for the forkJoin rxjs operator
 18) Write several karma/jasmine test cases that pass
 19) Create a typeahead input that uses rxjs switchMap
+20) Demonstrate use of rxjs combineLatest
 
 Commits correspond to each exercise
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router'
-import { debounceTime, distinctUntilChanged, Subscription, mergeMap, tap, switchMap, concatMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Subscription, mergeMap, tap, switchMap, concatMap, interval, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,36 +15,40 @@ export class AppComponent implements OnInit {
 
   applicationName = 'testApplication';
   randomProp = '';
-  retrievedNumbs= [];
 
   
-  
-  searchControl: FormControl = new FormControl("");
-  searchForm: FormGroup = new FormGroup({
-    searchInput: this.searchControl
-  });
+
 
   constructor(private testData: TestData, private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {
-    
-    let searchObs$ = this.searchForm.get("searchInput").valueChanges;
+    let obs1$ = interval(1000);
+    let obs2$ = interval(2000);
+    let obs3$ = interval(5000);
 
-    searchObs$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      tap(x => console.log(x)),
+    let counter = 0;
+    let intervalCounter = setInterval(() => {
+      counter++;
+      console.log("Seconds elapsed: " + counter);
+    }, 1000);
 
-      // will cancel if new val, before current request finishes
-      switchMap(searchVal => {
-        return this.testData.getDataTimeout(searchVal)
-      })
-    ).subscribe(searchData => {
-      console.log("returned results");
-      console.log(searchData);
-      this.retrievedNumbs = searchData;
+    let combinedSub = combineLatest([obs1$, obs2$, obs3$], (one, two, three) => {
+      return [one, two, three];
+    }).subscribe(allData => {
+      // notice that the first emission is when all observables emit at least one value (after 5 seconds)
+
+      console.log("obs1$: ", allData[0]);
+      console.log("obs2$: ", allData[1]);
+      console.log("obs3$: ", allData[2]);
     });
+
+    // finish after 10 seconds
+    setTimeout(() => {
+      combinedSub.unsubscribe();
+      clearInterval(intervalCounter);
+    }, 10000);
+
   }
 
   ngOnDestroy() {
